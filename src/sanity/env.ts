f28cf6e583e521c44e6aecb83e2773e061f21d05
Steve Austin Mollik <steveaustinmollik@gmail.com>
2026-01-28 15:04:39 +0600

    Update-Project-ID

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -2,5 +2,5 @@ export const apiVersion =
     process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-28'
 
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'kv87419s'; // Placeholder: User must update this!
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'n1hj1jwa';
 export const useCdn = false
